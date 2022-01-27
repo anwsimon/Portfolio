a430ea42d984d4d3f466ea1a042992b0c03bf906
anwsimon <anna.w.simon1@gmail.com>
2022-01-27 12:32:21 -0500

    deleted db

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const app = require('./app')
-const { db }= require("./db");
+// const { db }= require("./db");
 
 //for later deployment
 const PORT = process.env.PORT || 3000;
 
 
 //sync db then listen for requests
-db.sync({force: true})
-  .then(function(){
+const server = () => {
     app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
-  })
+  }
 
+  server()
