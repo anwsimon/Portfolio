0e9c3a46e3c4429b4f9972f438ded5b1f1034262
anwsimon <anna.w.simon1@gmail.com>
2022-01-27 12:27:54 -0500

    added favicon

--- server/db/db.js
+++
@@ -1,8 +0,0 @@
-//setting up new sequelize db
-
-
-const Sequelize = require('sequelize')
-
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/Portfolio', {logging: false})
-
-module.exports = db
