6b6726a17f07c2439d19e569bf101660e6dc7b4b
anwsimon <anna.w.simon1@gmail.com>
2023-06-09 13:27:29 -0400

    Update Navbar.js

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,25 +1,27 @@
-import * as React from 'react';
-
+import * as React from "react"
 
 const pages = [
-  ['Home', '/home'],
-  ['Projects', '/projects'],
-  ['Personal', '/personal']
-];
+  ["Home", "/home"],
+  ["Projects", "/projects"],
+]
 
 const Navbar = () => {
-
   return (
-    <div>
-    <img src="smiley.png" className="fixed h-20 w-20 object-cover"></img>
+    <div className="flex  ">
+      <img src="smiley.png" className="object-cover w-20 h-20"></img>
 
-    <nav className="flex sm:justify-center space-x-4">
-      {pages.map(([title, url]) => (
-        <a href={url} key={title} className="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-green hover:bg-green">{title}</a>
-      ))}
-    </nav>
-  </div>
-
+      <nav className="flex sm:justify-center space-x-4 p-4">
+        {pages.map(([title, url]) => (
+          <a
+            href={url}
+            key={title}
+            className="rounded-lg px-3 py-2 text-slate-700 text-lg hover:bg-green"
+          >
+            {title}
+          </a>
+        ))}
+      </nav>
+    </div>
   )
 }
-export default Navbar;
+export default Navbar
