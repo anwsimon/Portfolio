3614cbf44c6717fdbfa077d1c549ab1b2d44a460
anwsimon <anna.w.simon1@gmail.com>
2023-06-09 13:27:21 -0400

    Remove personal page

--- client/store/routes.js
+++ client/store/routes.js
@@ -1,20 +1,22 @@
-import React, { Component } from "react";
-import { Route, Routes} from "react-router-dom";
-import Home from '../components/Home'
-import ProjectsHome from "../components/ProjectsHome";
-import Personal from "../components/Personal";
-import ResponsiveAppBar from '../components/Navbar'
+import React, { Component } from "react"
+import { Route, Routes } from "react-router-dom"
+import Home from "../components/Home"
+import ProjectsHome from "../components/ProjectsHome"
+import ResponsiveAppBar from "../components/Navbar"
 class AppRoutes extends Component {
   render() {
     return (
       <div>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/projects" element={<ProjectsHome/>} />
-          <Route exact path="/projects/:projectName" element={<ResponsiveAppBar/>} />
-          <Route path="/personal" element={<Personal/>} />
-          <Route path="*" element={<Home/>} />
-       </Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<ProjectsHome />} />
+          <Route
+            exact
+            path="/projects/:projectName"
+            element={<ResponsiveAppBar />}
+          />
+          <Route path="*" element={<Home />} />
+        </Routes>
       </div>
     )
   }
