6b0c9dbb95f18f7364fb6414efe53fa7b5e11f72
anwsimon <anna.w.simon1@gmail.com>
2023-06-09 14:44:08 -0400

    Update homepage, add Basebar component

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,58 +1,26 @@
-import React from "react";
+import React from "react"
+import Basebar from "./Base"
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <div className="container mx-auto mt-8 md:mt-0 md:space-x-10 md:grid grid-cols-2 justify-center md:py-40">
+        <div className="container mx-auto mt-8 md:mt-0 md:space-x-10 md:space-y-10 md:grid grid-cols-2 justify-center md:py-40">
           <div className="grid justify-center items-center order-1 col-span-1">
-            <img
-              className="w-80 h-80 object-cover bg-bottom hover:border"
-              src="anna.JPG"
-              alt=""
-            />
+            <img className="w-80 h-80 object-cover" src="anna.JPG" alt="" />
           </div>
-          <div className="mt-8 md:mt-0 lg:justify-end justify-items-center col-span-1 items-center my-10">
-            <h1 className="text-4xl text-gray-800 text-center md:text-left font-bold mb-6">
+          <div className="mt-8 md:mt-0 lg:justify-end justify-items-center col-span-1 items-center my-10 max-w-xs">
+            <h1 className="text-4xl text-gray-800  md:text-left font-bold mb-6">
               Hello! I'm Anna.
             </h1>
-            <p className="text-xl mb-10text-gray-800 text-center justify-items-center md:text-left">
+            <p className="text-xl mb-10 text-gray-800 items-center md:text-left max-w-xs">
               I'm a software developer with a background in creative strategy. I
-              maintain a focus on the end-user in all of my work. I'm currently
-              working under NDA in the social justice space. I'd love to hear
-              from you!
+              love to work on supportive teams doing challenging work. Currently
+              under NDA in the social justice space. I'd love to hear from you!
             </p>
-            <div className="flex justify-center space-x-4 ">
-              <a href="mailto:anna.w.simon1@gmail.com">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="rounded-lg h-7 w-7 hover:bg-green "
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  />
-                </svg>
-              </a>
-              <a href="https://github.com/anwsimon">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="rounded-lg hover:bg-green "
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-                </svg>
-              </a>
-            </div>
           </div>
         </div>
+        <Basebar />
       </div>
     )
   }
