0e7b8fdeb1959f4995df3792eb98af10fe509b0c
anwsimon <anna.w.simon1@gmail.com>
2023-06-09 13:27:51 -0400

    Add loader for jpg file types

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,39 @@
-const path = require('path')
+const path = require("path")
 
 module.exports = {
-  mode: 'development',
-  entry: [
-    "./client/index.js"
-  ],
+  mode: "development",
+  entry: ["./client/index.js"],
   output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js',
+    path: __dirname + "/public",
+    filename: "bundle.js",
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          presets: [
-            '@babel/preset-react'
-          ]
-        }
+          presets: ["@babel/preset-react"],
+        },
       },
       {
         test: /\.css$/,
-        use:
-          [
-            'style-loader',
-            'css-loader'
-          ]
-      }
-    ]
-  }
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 1000,
+              name: "assets/img/[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
 }
